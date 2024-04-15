245b4fa22325dbc28bc5040c1f63b05a9997171a
Farragatao <155957078+Farragatao@users.noreply.github.com>
2024-04-15 08:19:07 +0200

    Add files via upload

---
+++ javaalberto.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<script>
+    var edad = prompt("Introduce tu edad");
+    if (Number(edad) == edad) {
+    if (edad >= 18) {
+    document.write("Puedes conducir");
+    }
+    }
+    else {
+    alert("Introduce un numero válido");
+    }
+    </script>
+
+<body>
+
+</body>
+</html>
---
+++ media numeros.js
@@ -0,0 +1,10 @@
+
+var num1 = 10;
+var num2 = 20;
+var num3 = 30;
+
+// Calcular la media
+var media = (num1 + num2 + num3) / 3;
+
+// Mostrar el resultado en la consola
+console.log("La media de los tres números es: " + media);
